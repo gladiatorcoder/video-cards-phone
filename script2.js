2e129ee6afa826ebab1426b10ae2a488c7ed2c9b
gladiatorcoder <fightfreeeak@gmail.com>
2023-06-07 13:07:55 +0530

    optimized scrolling

--- script2.js
+++ script2.js
@@ -84,11 +84,13 @@ const dragging = (e) => {
     if (positionDiff > 0) {
         carousel1.scrollBy({
             top: 0,
+            left: positionDiff * -1 - 200,
             behavior: 'smooth'
         })
     } else {
         carousel1.scrollBy({
             top: 0,
+            left: positionDiff * -1 + 200,
             behavior: 'smooth'
         })
     }
